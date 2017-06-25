b0405a6f2fd0233a8fb461e227fb5bd67bf6c099
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2017-06-25 11:27:57 -0500

    Adding files from matsim-playground boescpa.

---
+++ src/main/java/ch/ethz/matsim/boescpa/lib/tools/fileCreation/F2LConfigGroup.java
@@ -0,0 +1,9 @@
+package ch.ethz.matsim.boescpa.lib.tools.fileCreation;
+
+import org.matsim.core.config.ReflectiveConfigGroup;
+
+public class F2LConfigGroup extends ReflectiveConfigGroup {
+	public F2LConfigGroup() {
+		super("f2l", true);
+	}
+}
