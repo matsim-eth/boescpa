6343b401e685c0e903d9bcaa7c6e56bbf4ac31e7
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2017-06-28 12:49:34 -0500

    Initial set up of class to change network speeds by a given value.

---
+++ src/main/java/ch/ethz/matsim/boescpa/diss/baseline/calibration/NetworkSpeedsAdapter.java
@@ -0,0 +1,48 @@
+/*
+ * *********************************************************************** *
+ * project: org.matsim.*                                                   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2017 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** *
+ */
+
+package ch.ethz.matsim.boescpa.diss.baseline.calibration;
+
+import ch.ethz.matsim.boescpa.lib.tools.NetworkUtils;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.NetworkWriter;
+
+/**
+ * WHAT IS IT FOR?
+ *
+ * @author boescpa
+ */
+public class NetworkSpeedsAdapter {
+
+	public static void main(final String[] args) {
+		Network network = NetworkUtils.readNetwork(args[0]);
+		double speedReduction = Double.parseDouble(args[1])/3.6; // reduction in [km/h]
+
+		for (Link link : network.getLinks().values()) {
+			if (link.getAllowedModes().contains("car"))
+				link.setFreespeed(link.getFreespeed() - speedReduction);
+		}
+
+		new NetworkWriter(network).write("network_reducedSpeed.xml.gz");
+	}
+
+}
