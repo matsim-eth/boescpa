36ea4eb086abdebce714678005afb7404c14d3b2
boescpa <patrick.boesch@ivt.baug.ethz.ch>
2017-12-14 17:39:13 +0100

    Extending and debugging.

--- src/main/java/ch/ethz/matsim/boescpa/diss/av/dynamicFleet/delayedDeployment/analysis/SavFleetStats.java
+++ src/main/java/ch/ethz/matsim/boescpa/diss/av/dynamicFleet/delayedDeployment/analysis/SavFleetStats.java
@@ -21,12 +21,18 @@
 
 package ch.ethz.matsim.boescpa.diss.av.dynamicFleet.delayedDeployment.analysis;
 
+import ch.ethz.matsim.boescpa.lib.tools.coordUtils.CoordAnalyzer;
 import ch.ethz.matsim.boescpa.lib.tools.utils.NetworkUtils;
+import ch.ethz.matsim.boescpa.lib.tools.utils.PopulationUtils;
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.*;
 import org.matsim.api.core.v01.events.handler.*;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
@@ -47,22 +53,32 @@ import java.util.Set;
  * @author boescpa
  */
 public class SavFleetStats {
-	private final static int LoS = 300;
 	private final Network network;
+	private final int LoS;
+	private final CoordAnalyzer coordAnalyzer;
+	private final Population population;
 
-	public SavFleetStats(Network network) {
+	public SavFleetStats(Network network, Population population, int LoS, String path2SHP) {
 		this.network = network;
+		this.population = population;
+		this.LoS = LoS;
+		coordAnalyzer = path2SHP != null ? CoordAnalyzer.getCoordAnalyzer(path2SHP) : null;
 	}
 
 	public static void main(final String[] args) throws IOException {
 		String path2Events = args[0];
 		String path2Network = args[1];
+		String path2Population = args[2];
+		int LoS = Integer.parseInt(args[3]);
+		double scalingFactor = Integer.parseInt(args[4]);
+		String path2SHP = args.length > 5 ? args[5] : null;
 
-		SavFleetStats misc2 = new SavFleetStats(NetworkUtils.readNetwork(path2Network));
-		misc2.handleEvents(path2Events);
+		SavFleetStats misc2 = new SavFleetStats(NetworkUtils.readNetwork(path2Network),
+				PopulationUtils.readPopulation(path2Population), LoS, path2SHP);
+		misc2.handleEvents(path2Events, scalingFactor);
 	}
 
-	private void handleEvents(String path2Events) throws IOException {
+	private void handleEvents(String path2Events, double scalingFactor) throws IOException {
 		MyHandler handler = new MyHandler();
 		EventsManager eventsManager= EventsUtils.createEventsManager();
 		eventsManager.addHandler(handler);
@@ -72,78 +88,109 @@ public class SavFleetStats {
 		if (handler.linkLeaves > 500) System.out.println(handler.lastTime + ": " + handler.linkLeaves);
 
 		BufferedWriter writer = IOUtils.getBufferedWriter("./savUsageStats.csv");
-		writer.write("\nTotal waiting time agents; " + handler.totalWaitingTime);
+		writer.write("\nTotal waiting time agents; " + handler.totalWaitingTime
+				+ "; " + scalingFactor*handler.totalWaitingTime);
 		writer.newLine();
-		writer.write("Average waiting time; " + handler.totalWaitingTime/handler.numberOfPassengers);
+		writer.write("Average waiting time; " + handler.totalWaitingTime/handler.numberOfPassengers
+				+ "; " + handler.totalWaitingTime/handler.numberOfPassengers);
 		writer.newLine();
-		writer.write("Max waiting time; " + handler.maxWaitingTime);
+		writer.write("Max waiting time; " + handler.maxWaitingTime
+				+ "; " + handler.maxWaitingTime);
 		writer.newLine();
-		writer.write("Min waiting time; " + handler.minWaitingTime);
+		writer.write("Min waiting time; " + handler.minWaitingTime
+				+ "; " + handler.minWaitingTime);
 		writer.newLine();
-		writer.write("Number of waiting times bigger LoS; " + handler.anzWaitingTimeBigger180);
+		writer.write("Number of waiting times bigger LoS; " + handler.anzWaitingTimeBigger180
+				+ "; " + scalingFactor*handler.anzWaitingTimeBigger180);
 		writer.newLine();
 
-		writer.write("\nTotal pick-up drive time; " + handler.totalPickTime);
+		writer.write("\nTotal pick-up drive time; " + handler.totalPickTime
+				+ "; " + scalingFactor*handler.totalPickTime);
 		writer.newLine();
 		//writer.write("Average pick-up drive time per passenger (only aTaxi); " + handler.totalPickTime/handler.numberOfPassengers);
 		//writer.newLine();
-		writer.write("Average pick-up drive time per SAV and day; " + handler.totalPickTime/handler.avVehicles.size());
+		writer.write("Average pick-up drive time per SAV and day; " + handler.totalPickTime/handler.avVehicles.size()
+				+ "; " + handler.totalPickTime/handler.avVehicles.size());
 		writer.newLine();
 		//writer.write("Average pick dist per passenger (only aTaxi); " + handler.totalPickDist/handler.numberOfPassengers);
 		//writer.newLine();
-		writer.write("Max pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.maxPickDist);
+		writer.write("Max pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.maxPickDist
+				+ "; " + handler.maxPickDist);
 		writer.newLine();
-		writer.write("Min pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.minPickDist);
+		writer.write("Min pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.minPickDist
+				+ "; " + handler.minPickDist);
 		writer.newLine();
-		writer.write("Average pick dist per SAV and day; " + handler.totalPickDist/handler.avVehicles.size());
+		writer.write("Average pick dist per SAV and day; " + handler.totalPickDist/handler.avVehicles.size()
+				+ "; " + handler.totalPickDist/handler.avVehicles.size());
 		writer.newLine();
 		writer.write("Average crow pick dist to passenger (aTaxi) or first passenger of pool (aRS); "
-				+ handler.totalCrowPickDist/handler.numberOfPassengers);
+				+ handler.totalCrowPickDist/handler.numberOfPassengers
+				+ "; " + handler.totalCrowPickDist/handler.numberOfPassengers);
 		writer.newLine();
-		writer.write("Max crow pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.maxCrowPickDist);
+		writer.write("Max crow pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.maxCrowPickDist
+				+ "; " + handler.maxCrowPickDist);
 		writer.newLine();
-		writer.write("Min crow pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.minCrowPickDist);
+		writer.write("Min crow pick dist to passenger (aTaxi) or first passenger of pool (aRS); " + handler.minCrowPickDist
+				+ "; " + handler.minCrowPickDist);
 		writer.newLine();
-		writer.write("Anz crow pick dist bigger LoS; " + handler.anzCrowDistBigger180);
-		writer.newLine();
-
-		writer.write("\nTotal boarding time; " + handler.totalBoardingTime);
-		writer.newLine();
-		writer.write("Average boarding time; " + handler.totalBoardingTime/handler.numberOfPassengers);
-		writer.newLine();
-		writer.write("Max boarding time; " + handler.maxBoardingTime);
-		writer.newLine();
-		writer.write("Min boarding time; " + handler.minBoardingTime);
-		writer.newLine();
-		writer.write("Average boarding time per SAV and day; " + handler.totalBoardingTime/handler.avVehicles.size());
+		writer.write("Number of crow pick dist bigger LoS; " + handler.anzCrowDistBigger180
+				+ "; " + scalingFactor*handler.anzCrowDistBigger180);
 		writer.newLine();
 
-		writer.write("\nTotal driving time; " + handler.totalDrivingTime);
+		writer.write("\nTotal boarding time; " + handler.totalBoardingTime
+				+ "; " + scalingFactor*handler.totalBoardingTime);
 		writer.newLine();
-		writer.write("Average driving time per passenger; " + handler.totalDrivingTimePassengers/handler.numberOfPassengers);
+		writer.write("Average boarding time; " + handler.totalBoardingTime/handler.numberOfPassengers
+				+ "; " + handler.totalBoardingTime/handler.numberOfPassengers);
 		writer.newLine();
-		writer.write("Average driving time per SAV and day; " + handler.totalDrivingTime/handler.avVehicles.size());
+		writer.write("Max boarding time; " + handler.maxBoardingTime
+				+ "; " + handler.maxBoardingTime);
 		writer.newLine();
-		writer.write("Total driving distance; " + handler.totalDrivingDist);
+		writer.write("Min boarding time; " + handler.minBoardingTime
+				+ "; " + handler.minBoardingTime);
 		writer.newLine();
-		writer.write("Average driving distance per SAV and day; " + handler.totalDrivingDist/handler.avVehicles.size());
+		writer.write("Average boarding time per SAV and day; " + handler.totalBoardingTime/handler.avVehicles.size()
+				+ "; " + handler.totalBoardingTime/handler.avVehicles.size());
+		writer.newLine();
+
+		writer.write("\nTotal driving time; " + handler.totalDrivingTime
+				+ "; " + scalingFactor*handler.totalDrivingTime);
+		writer.newLine();
+		writer.write("Average driving time per passenger; " + handler.totalDrivingTimePassengers/handler.numberOfPassengers
+				+ "; " + handler.totalDrivingTimePassengers/handler.numberOfPassengers);
+		writer.newLine();
+		writer.write("Average driving time per SAV and day; " + handler.totalDrivingTime/handler.avVehicles.size()
+				+ "; " + handler.totalDrivingTime/handler.avVehicles.size());
+		writer.newLine();
+		writer.write("Total driving distance; " + handler.totalDrivingDist
+				+ "; " + scalingFactor*handler.totalDrivingDist);
+		writer.newLine();
+		writer.write("Average driving distance per SAV and day; " + handler.totalDrivingDist/handler.avVehicles.size()
+				+ "; " + handler.totalDrivingDist/handler.avVehicles.size());
 		writer.newLine();
 		writer.write("Average driving speed AV when transporting a passenger; "
-				+ handler.totalDrivingDist/handler.totalDrivingTime);
+				+ handler.totalDrivingDist/handler.totalDrivingTime
+				+ "; " + handler.totalDrivingDist/handler.totalDrivingTime);
 		writer.newLine();
 
-		writer.write("\nTotal dropoff time; " + handler.totalDropOffTime);
+		writer.write("\nTotal dropoff time; " + handler.totalDropOffTime
+				+ "; " + scalingFactor*handler.totalDropOffTime);
 		writer.newLine();
-		writer.write("Average dropoff time per passenger; " + handler.totalDropOffTime/handler.numberOfPassengers);
+		writer.write("Average dropoff time per passenger; " + handler.totalDropOffTime/handler.numberOfPassengers
+				+ "; " + handler.totalDropOffTime/handler.numberOfPassengers);
 		writer.newLine();
-		writer.write("Average dropoff time per SAV and day; " + handler.totalDropOffTime/handler.avVehicles.size());
+		writer.write("Average dropoff time per SAV and day; " + handler.totalDropOffTime/handler.avVehicles.size()
+				+ "; " + handler.totalDropOffTime/handler.avVehicles.size());
 		writer.newLine();
 
-		writer.write("\nTotal number of passengers; " + handler.numberOfPassengers);
+		writer.write("\nTotal number of passengers; " + handler.numberOfPassengers
+				+ "; " + scalingFactor*handler.numberOfPassengers);
 		writer.newLine();
-		writer.write("Number of SAVs; " + handler.avVehicles.size());
+		writer.write("Number of SAVs; " + handler.avVehicles.size()
+				+ "; " + scalingFactor*handler.avVehicles.size());
 		writer.newLine();
-		writer.write("Number of passengers per SAV; " + handler.numberOfPassengers/handler.avVehicles.size());
+		writer.write("Number of passengers per SAV; " + handler.numberOfPassengers/handler.avVehicles.size()
+				+ "; " + handler.numberOfPassengers/handler.avVehicles.size());
 		writer.newLine();
 		writer.close();
 
@@ -207,7 +254,8 @@ public class SavFleetStats {
 		// ... then the person boards which takes some time...
 		@Override
 		public void handleEvent(PersonEntersVehicleEvent personEntersVehicleEvent) {
-			if (personDepartureTime.containsKey(personEntersVehicleEvent.getPersonId())) {
+			if (personDepartureTime.containsKey(personEntersVehicleEvent.getPersonId())
+					&& personHomeInArea(personEntersVehicleEvent.getPersonId())) {
 				double waitingTime = personEntersVehicleEvent.getTime() -
 						personDepartureTime.get(personEntersVehicleEvent.getPersonId());
 				totalWaitingTime += waitingTime;
@@ -243,8 +291,10 @@ public class SavFleetStats {
 
 		@Override
 		public void handleEvent(PersonLeavesVehicleEvent personLeavesVehicleEvent) {
-			if (personDrivingTime.containsKey(personLeavesVehicleEvent.getPersonId())) {
-				totalDrivingTimePassengers += personDrivingTime.remove(personLeavesVehicleEvent.getPersonId());
+			if (personDrivingTime.containsKey(personLeavesVehicleEvent.getPersonId())
+					&& personHomeInArea(personLeavesVehicleEvent.getPersonId())) {
+				totalDrivingTimePassengers += personLeavesVehicleEvent.getTime()
+						- personDrivingTime.remove(personLeavesVehicleEvent.getPersonId());
 			}
 		}
 
@@ -280,7 +330,8 @@ public class SavFleetStats {
 				vehiclePickUpDist.put(activityEndEvent.getPersonId().toString(), 0d);
 				vehiclePickUpTime.put(activityEndEvent.getPersonId().toString(), activityEndEvent.getTime());
 				vehicleCrowDist.put(activityEndEvent.getPersonId().toString(), activityEndEvent.getLinkId().toString());
-			} else if (activityEndEvent.getActType().equals("AVDropoff")) {
+			} else if (activityEndEvent.getActType().equals("AVDropoff") &&
+					vehicleDriveTime.containsKey(activityEndEvent.getPersonId().toString())) {
 				totalDropOffTime += 10;
 			}
 		}
@@ -322,5 +373,20 @@ public class SavFleetStats {
 
 		@Override
 		public void reset(int i) {}
+	}
+
+	private boolean personHomeInArea(Id<Person> personId) {
+		if (coordAnalyzer == null) return true;
+		Person person = this.population.getPersons().get(personId);
+		for (PlanElement planElement : person.getSelectedPlan().getPlanElements()) {
+			if (planElement instanceof Activity) {
+				Activity activity = (Activity) planElement;
+				if (activity.getType().equals("home_1")) {
+					Coord homeCoord = activity.getCoord();
+					return coordAnalyzer.isCoordAffected(homeCoord);
+				}
+			}
+		}
+		return false;
 	}
 }
